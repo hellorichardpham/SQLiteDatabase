b7e9834e9df430ed800d0f5341b60d4ce7fb8db6
Richard Pham <hellorichardpham@gmail.com>
2014-07-30 20:36:22 -0700

    Loop, Find Last Index
    Added a while loop to ask user if they'd like to make another action.
    Added a statement to determine the last index of the database so we can
    add straight to the end of the database.

--- src/UtilizeTableSQL.java
+++ src/UtilizeTableSQL.java
@@ -1,50 +1,81 @@
 import java.sql.*;
 import java.util.Scanner;
 
+/*The next thing that I want to do is to generate the last ID of the table so that I can automatically 
+ add it to the command 
+ */
 public class UtilizeTableSQL {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
 		Connection c = null;
+
 		Statement stmt = null;
-		
-		try {
-			Class.forName("org.sqlite.JDBC");
-			c = DriverManager.getConnection("jdbc:sqlite:gradebook.db");
-			c.setAutoCommit(false);
-	
-			stmt = c.createStatement();
-	
-			String userAction = promptUserAction();
-			String sql = generateSQLCommand(userAction);
-			System.out.println(sql);
-			stmt.executeUpdate(sql);
-			
-			c.commit();
-			stmt.close();
-			c.close();
-			System.out.println("Action Successful.");
-		} catch(Exception e) {
-			//do something
+		boolean cont = true;
+		while (cont) {
+			try {
+				Class.forName("org.sqlite.JDBC");
+				c = DriverManager.getConnection("jdbc:sqlite:gradebook.db");
+				c.setAutoCommit(false);
+				stmt = c.createStatement();
+				/*The purpose of lastID is solely for adding so we always keep track 
+				 * of the last ID and can add to the next one.
+				 */
+				ResultSet rs = stmt.executeQuery(getLastIDNumber());
+				int lastID = rs.getInt("ID");
+				String userAction = promptUserAction();
+				String sql = generateSQLCommand(userAction, lastID, stmt);
+				stmt.executeUpdate(sql);
+				c.commit();
+				stmt.close();
+				c.close();
+				System.out
+						.println("Action Successful. Would you like to continue? (Yes or No)");
+				cont = determineContinue();
+			} catch (Exception e) {
+				System.err.println(e.getClass().getName() + ": "
+						+ e.getMessage());
+			}
+		}
+
+	}
+
+	private static boolean determineContinue() {
+		Scanner scan = new Scanner(System.in);
+		while (true) {
+			String decision = scan.nextLine().toLowerCase();
+			switch (decision) {
+			case "yes":
+				return true;
+			case "no":
+				System.out.println("This application will now close.");
+				return false;
+			default:
+				System.out
+						.println("That is an invalid command. Please indicate \"Yes\" or \"No\".");
+			}
 		}
 	}
-	
-	private static String generateSQLCommand(String userAction) {
-		switch(userAction) {
+
+	private static String generateSQLCommand(String userAction, int lastID,
+			Statement stmt) throws Exception {
+		switch (userAction) {
 		case "update":
 			return generateUpdate();
-		
+
 		case "select":
-			return generateSelect();
-			
+			return generateSelect(stmt);
+
 		case "insert":
-			return generateInsert();
-		
+			return generateInsert(lastID);
+
 		case "delete":
 			return generateDelete();
 		}
 		return "";
 	}
+
 	private static String promptUserAction() {
 		Scanner scan = new Scanner(System.in);
 		System.out
@@ -88,8 +119,8 @@ public class UtilizeTableSQL {
 			return "UPDATE Grades set " + categoryToChange + "=" + value
 					+ " WHERE " + categoryToCheck + "= '" + valueToCheck + "';";
 		} else {
-			// This case is where we are updating "Name" or "Comments" so we
-			// need a ' ' around value
+			/* This case is where we are updating "Name" or "Comments" so we
+			need a ' ' around value */
 			return "UPDATE Grades set " + categoryToChange + "=" + "'" + value
 					+ "'" + " WHERE " + categoryToCheck + "= '" + valueToCheck
 					+ "';";
@@ -112,12 +143,11 @@ public class UtilizeTableSQL {
 		}
 	}
 
-	public static String generateInsert() {
+	public static String generateInsert(int lastID) {
 		Scanner scanNumbers = new Scanner(System.in);
 		Scanner scanStrings = new Scanner(System.in);
 
-		System.out.println("Please input an ID.");
-		int ID = scanNumbers.nextInt();
+		int nextIDNumber = lastID + 1;
 		System.out.println("Please input a name.");
 		String name = scanStrings.nextLine().toLowerCase();
 		System.out.println("Please input a grade.");
@@ -126,15 +156,34 @@ public class UtilizeTableSQL {
 				+ ". Type \"None\" if there are none");
 		String comments = scanStrings.nextLine().toLowerCase();
 
-		return "INSERT INTO GRADES(ID,NAME,GRADE,COMMENTS)" + "VALUES (" + ID
-				+ ", " + "'" + name + "', " + grade + ", '" + comments + "' );";
+		return "INSERT INTO GRADES(ID,NAME,GRADE,COMMENTS)" + "VALUES ("
+				+ nextIDNumber + ", " + "'" + name + "', " + grade + ", '"
+				+ comments + "' );";
 	}
-	
-	public static String generateSelect() {
+
+	public static String generateSelect(Statement stmt) throws Exception {
 		Scanner scan = new Scanner(System.in);
 		System.out
 				.println("Please indicate the name of the student that you would like to access.");
-		String name = scan.nextLine();
-		return "SELECT * FROM Grades WHERE NAME = '" + name + "';";
+		String targetName = scan.nextLine();
+
+		ResultSet rs = stmt.executeQuery("SELECT * FROM Grades WHERE NAME = '"
+				+ targetName + "';");
+		int ID = rs.getInt("ID");
+		String name = rs.getString("NAME");
+		int grade = rs.getInt("GRADE");
+		String comments = rs.getString("COMMENTS");
+		printStudentInfo(ID, name, grade, comments);
+		return "";
+	}
+
+	public static String getLastIDNumber() {
+		return "SELECT * FROM Grades ORDER BY ID DESC LIMIT 1;";
+	}
+
+	public static void printStudentInfo(int ID, String name, int grade,
+			String comments) {
+		System.out.printf("ID: %d \nName: %s \nGrade: %d \nComments: %s \n",
+				ID, name, grade, comments);
 	}
 }
